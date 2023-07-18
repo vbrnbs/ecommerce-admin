58b5dfdd2554967bc6b540ba3f09a65f71805160
vbrnbs <varszegibarnabas@gmail.com>
2023-07-18 17:19:17 +1200

    clerk auth sign in/up is added with layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
     return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <Button>Hello</Button>
-      </main>
     )
   }
   
